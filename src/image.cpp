9a9fd346adde8ba4a91aca1844f3b1758c15cb04
113511007 <juniorlin0123@gmail.com>
2025-05-31 03:20:04 +0800

    step4-encryption

--- src/image.cpp
+++ src/image.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 Image::Image(int theWidth, int theHeight, int theChannels): width(theWidth), height(theHeight), channels(theChannels) {}
 Image::~Image() {}
+bool Image::LoadImage(string filename) { return false; }
+void Image::DumpImage(string filename) {}
+void Image::Display_X_Server() {}
+void Image::Display_ASCII() {}
+void Image::Display_CMD() {}
 int Image::get_width() {return width;}
 int Image::get_height() {return height;}
 int Image::get_channels() {return channels;}
